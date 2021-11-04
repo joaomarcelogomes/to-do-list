427728de134dc5f3d52387850d919fac52738a33
João Marcelo Gomes <joaomarcelosgomes@gmail.com>
2021-11-04 20:13:23 -0300

    Aula Models e Rotas

--- src/routes/checklists.js
+++ src/routes/checklists.js
@@ -2,24 +2,37 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req,res) =>{   
-    console.log('Olá')
-    res.send()
+const Checklist = require('../models/checklist')
+
+router.get('/', async (req,res) =>{   
+    try {
+        let checklists = await Checklist.find({})
+        res.status(200).json(checklists)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
-//outra forma possível: router.get('/', (req,res) =>{   
-    //console.log('Olá')
-    //res.send()
-//})
+router.post('/', async (req,res) => {
+    let { name } = req.body
+    
+    try {
+        let checklist = await Checklist.create({ name })
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
 
-router.post('/', (req,res) => {
-    console.log(req.body)
     res.status(200).send(req.body)
 })
 
-router.get('/:id', (req,res) => {
-    console.log(req.params.id)
-    res.send(`ID: ${req.params.id}`)
+router.get('/:id', async (req,res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id)
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
 })
 
 router.put('/:id', (req,res) => {
