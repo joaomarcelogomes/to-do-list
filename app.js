69941c31ca1e62e1688ee70c24f0fe5cc10027e6
João Marcelo Gomes <joaomarcelosgomes@gmail.com>
2021-11-03 22:35:59 -0300

    Aula de introduçao ao Mongoose

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express')
-
 const app = express()
-
 const checklistRouter = require('./src/routes/checklists')
+require('./config/database')
 
 app.use(express.json())
 
-app.use('/checklists', checklistRouter) //ou, app.use('/checklists', checklistRouter)
+app.use('/checklists', checklistRouter) //ou, app.use('/', checklistRouter)
 
 app.listen(3000, () => {
     console.log('Servidor iniciado') 
