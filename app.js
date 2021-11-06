aec357866e6de470087eee7301ef7c51ef8efdfa
João Marcelo Gomes <joaomarcelosgomes@gmail.com>
2021-11-06 06:36:37 -0300

    Introduçao ao EJS

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const checklistRouter = require('./src/routes/checklists')
+const rootRouter = require('./src/routes/index')
+const path = require('path')
+
 require('./config/database')
+
+app.set('views', path.join(__dirname, 'src/view'))
+app.set('view engine', 'ejs')
 
 app.use(express.json())
 
+app.use('/', rootRouter)
 app.use('/checklists', checklistRouter) //ou, app.use('/', checklistRouter)
 
 app.listen(3000, () => {
